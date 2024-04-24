f0d35c154cf4bf6b92499f17c2712a062d455e7b
Imran Farooq <imranf620@gmail.com>
2024-04-24 12:27:58 +0500

    full

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import "./App.css"
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Main from './Pages/Main/Main'
-import Testing from './Pages/Test/Testing';
+
 
 const App = () => {
   return (
